703783705613e45f96d5cbb2915b04f02e865644
unknown <syifafdhlh>
2013-09-20 14:04:55 +0700

    sesi3

--- sesii3/person2.java
+++ sesii3/person2.java
@@ -1,10 +1,20 @@
 class Person{
 			public static void main(String[] args) {
-			int a;
-			a = 1000;
-			int b = 1000;
-				System.out.println("a = " + a);
-				System.out.println("b = " + b);
-				System.out.println("a + b = " + (a + b));
+			byte number1 = 12;
+			short number2 = 100;
+			int number3 = 1000;
+			long number4 = 20001;
+			float number5 = 120023.45f;
+			double number6 = 123456;
+			boolean value = true;
+			char character = 'a';
+				System.out.println(number1);
+				System.out.println(number2);
+				System.out.println(number3);
+				System.out.println(number4);
+				System.out.println(number5);
+				System.out.println(number6);
+				System.out.println(value);
+				System.out.println(character);
 }
 }
